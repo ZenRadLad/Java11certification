deaa785e96bd74f8aa1cec1214643041b2ccd934
ZenRadLad <175264@supinfo.com>
2020-11-25 23:55:52 +0100

    Wrapper classes - Autoboxing and unboxing

--- src/main/java/dataTypes/Primitives.java
+++ src/main/java/dataTypes/Primitives.java
@@ -5,6 +5,7 @@ public class Primitives {
 	public static void main(String[] args) {
 		// https://docs.oracle.com/javase/tutorial/java%nutsandbolts/datatypes.html
 		primitives();
+		wrappers();
 	}
 
 	// Primitives
@@ -36,6 +37,10 @@ public class Primitives {
 
 	public static void primitives() {
 		// Numberic
+		byte maxB = 127;
+		byte minB = -128;
+		System.out.printf("byte min value : %d, max : %d%n", maxB, minB);
+		
 		short minShort = 32767;
 		short maxShort = -32767;
 		System.out.printf("short min value : %d, max : %d%n", minShort, maxShort);
@@ -58,22 +63,66 @@ public class Primitives {
 		System.out.printf("double min value : %f, max : %f%n", minDouble, maxDouble);
 
 		// Textual
-		byte maxB = 127;
-		byte minB = -128;
-		System.out.printf("byte min value : %d, max : %d%n", maxB, minB);
-
 		char myCh = 'a'; // unicode chars
 		char code = 122;
 		char hexUnicode = '\u0247';
 		System.out.printf("char single : %s, code : %s, unicode : %s%n", myCh, code, hexUnicode);
 
 		boolean bool = null != null; // true || false
-		System.out.printf("boolean : is null != null = %s%n", bool);
+		System.out.printf("boolean : is null != null = %s%n%n", bool);
+		System.out.print("-----------------------------------------------------");
+	    System.out.println("");
+	    System.out.println("");
 	}
 
-	// Casting
 	
 	// Wrapper Classes
+	public static void wrappers() {
+		// Numberic
+		Byte byteVal = Byte.valueOf("9");
+		System.out.printf("Byte wrapper => Byte.valueOf(\"9\") byteValue() : %d%n", byteVal.byteValue());
+		
+		Short myShort = 89;
+		System.out.printf("Short wrapper => Short myShort = 89 shortValue() : %d%n", myShort.shortValue());
+
+		Integer myInt = Integer.valueOf(Integer.MAX_VALUE);
+		System.out.printf("Integer wrapper => Integer.valueOf(Integer.MAX_VALUE) longValue() : %d%n", myInt.intValue());
+
+		Long myLong = Long.valueOf(20l);
+		System.out.printf("Long wrapper => Long.valueOf(20l) : %d%n", myLong.longValue());
+
+		Float myFloat = 699.19f;
+		System.out.printf("Float wrapper => Float myFloat = 699.19f  floatValue(): %f%n", myFloat.floatValue());
+
+		Double myDouble = 699.19;
+		System.out.printf("Double wrapper => Double myDouble = 699.19 doubleValue() : %f%n", myDouble.doubleValue());
+
+		// Textual
+		Boolean myBool = Boolean.valueOf("true");
+		System.out.printf("Boolean wrapper => Boolean myBool = Boolean.valueOf(\"true\") booleanValue() : %s%n", myBool.booleanValue());
+		
+		Character myChar = 'A';
+		System.out.printf("Character wrapper => Character myChar = 'A' charValue() : %s%n", myChar.charValue());
+		
+		//AutoBoxing
+		Double autoboxDouble = 99.99;
+		System.out.printf("Autoboxing 99.99 literal value in an Double object : %f%n", autoboxDouble.doubleValue());
+		
+		//Caching
+		Float floatObj = 11f;
+		Float sameCachedFloatObj = Float.valueOf(11f);
+		System.out.printf("Autoboxing of values from -128 to 127 "
+				+ "true and false , and '\\u0000' to '\\u007f' will result in cached "
+				+ "objects (refering to the same object) %n" 
+				+ "   ==> Float.valueOf(11f) and Float floatObj = 11f hashcodes : (%d == %s) %n", floatObj.hashCode(), sameCachedFloatObj.hashCode() );
+
+		//Unboxing
+		Integer unboxInteger = Integer.valueOf(2);
+		int unboxedInt = unboxInteger;
+		System.out.printf("Unboxing Integer unboxInteger = Integer.valueOf(2) to int unboxedInt = unboxInteger : %s%n", unboxedInt);
+	}
+	
+	// Casting
 
 	// Operators
 
